833d6b4322b3fe425394e76326a9f2de86c1a08e
RS8077 <sharmarohit8077@gmail.com>
2022-07-17 11:12:49 +0530

    change and modify it as you can

---
+++ main.py
@@ -0,0 +1,51 @@
+""" this is a comment """
+#print("hello \\ world \t")
+#thsi is a comment
+#variables in python
+#var_1 = "12"
+#var2 = 0.4
+#print(var_1*var2)
+#print(type(var2))
+#var9 = "hello world"
+#print(var9 + var_1) adding strings
+
+#TYPECASTING:
+#print(int(var_1) + str(var9))
+#print(10*var9  )
+"""val1 = int(input("enter the value1 : ")) code for taking input from user:
+print("you have entered :",val1)
+val2 = int(input("enter thr second value:"))
+print("you have entered :",val2)
+sum  = val1 + val2
+print(sum)"""
+
+
+#list in python
+#name = ["nitin","rohit","sarthak"]
+#print(len(name))
+"""grocery = ["soap", "tide","shampoo","hair wax"]
+print(grocery[3])
+numbers = [1,5,9,7,8,33,21]
+print(numbers)
+numbers.insert(1,67)
+print(numbers)
+#numbers.sort()
+#numbers.reverse()
+#print(numbers)"""
+
+
+#extendded slice
+#code: print( numbers[::2])
+"""this slicing skips these numbers after the colon"""
+# append is used to add a number in a list at the end os list;
+#numbers.append() this piece of code is used for this:
+#numbers.insert(1,67) is a code used to insert a number in the list
+#number .remove(the number to be removed)
+#numbers.pop()
+#numbers.push()
+
+tp = (1,12,344,5566,90,80)
+print(tp)
+print(type(tp))
+print(len(tp))
+
---
+++ tut.py
@@ -0,0 +1,3 @@
+d1 = {}
+print(type(d1))
+print("hello world")
